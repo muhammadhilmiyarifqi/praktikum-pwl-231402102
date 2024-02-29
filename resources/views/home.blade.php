c07f830b546d2ef81906a2179ce529a2427e70c5
unknown <hilmiy.arifqi77@gmail.com>
2024-02-29 15:39:51 +0700

    tugas 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <p class="text-3xl bg-blue-500"> hai guyss </p>
+    <div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Shoes!
+      <div class="badge badge-secondary hover:bg-blue-900">NEW</div>
+    </h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-outline">Fashion</div> 
+      <div class="badge badge-outline">Products</div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
